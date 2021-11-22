f5b9dbfcd593a1e3dd110aeedcf872800b555d5a
Adrenalin <vadimtsh@gmail.com>
2021-11-22 22:07:46 +0300

    addLesson_2

--- src/main/java/Lesson_3/Counter.java
+++ src/main/java/Lesson_3/Counter.java
@@ -4,14 +4,14 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Counter {
-    private static final Lock l = new ReentrantLock();
-    private static int counter = 0;
+    private final Lock l = new ReentrantLock();
+    private int counter = 0;
 
-    public static int getCounter() {
+    public  int getCounter() {
         return counter;
     }
 
-    public static void increment() {
+    public  void increment() {
         l.lock();
         counter++;
         l.unlock();
